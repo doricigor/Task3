65aa06de778a91f1cec0f1ff1f209744521d824b
Igor Doric <doricigor@yahoo.com>
2020-02-03 08:40:56 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// const product = document.querySelector('.product');
+
+//     productHTML = `<div class="product__star"><i class="far fa-star"></i></div>
+//                 <div class="product__img"><img src="${img}" alt="sat"></div>
+//                 <div class="product__brand">${brandName} <span class="product__subbrand">${SubBrandName}</span>
+//                 </div>
+//                 <div class="product__itemNo">${itemNo}</div>
+//                 <div class="product__listing">${listing}</div>
+//                 <div class="product__wholesale">${wholesale}</div>
+//                 <div class="product__qty">
+//                     <input type="number" class="product__qtyNum" value="0" min="0">
+//                 </div>
+//                 <div class="product__add"><a href="#" class="addBtn">ADD</a></div>
+//                 <div class="product__arrow"><a data-toggle="collapse" href="#product1" aria-expanded="false">
+//                 <i class="fas fa-chevron-down"></i></a></div>`;
+
+// product.insertAdjacentHTML('afterbegin', productHTML);
