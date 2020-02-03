36ed0bc82b5f7e980dbf03d369c5a41b8b476aa1
Igor Doric <doricigor@yahoo.com>
2020-02-04 00:32:48 +0100

    JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// TODO: 
+// - Obrisati odabrani proizvod iz niza na removeItem;
+// - Smanjiti iznos na smanjenje kolicine prozivoda;
+// - Ako je kolicina 0 ne moze se dodati u korpu;
+// - Kada se klikne dugme odabranog proizvoda ne moze se klikati opet, prelazi u added;
+// - Kada se menja kolicina azurira se total cancelAnimationFrame;
 
 // Variables
 const stars = document.querySelectorAll('.product__star');
@@ -7,7 +13,9 @@ const hoverCart = document.querySelector('.header__right');
 const myCart = document.querySelector('.mycart');
 const add = document.querySelectorAll('.addBtn');
 const del = document.querySelector('.mycart__list');
-
+const totalTop = document.querySelector('.header__cash');
+const totalBottom = document.querySelector('.total');
+const totalItem = document.querySelector('.header__totalItem');
 
 // Listeners
 openMyCart.addEventListener('click', openCart);
@@ -16,6 +24,13 @@ hoverCart.addEventListener('mouseleave', closePopUp);
 del.addEventListener('click', removeItem);
 
 // Functions
+
+
+// Currency format
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
 
 // Like and dislike of product
 stars.forEach(star => {
@@ -36,13 +51,13 @@ function collapseInfo(el) {
 
 // Open popup when item is added
 function openPopUp() {
-    const addedCart = this.children[1];
+    const addedCart = this.children[2];
     addedCart.classList.toggle('added__cart--active');
 }
 
 // Close popup when item is added
 function closePopUp() {
-    const addedCart = this.children[1];
+    const addedCart = this.children[2];
     addedCart.classList.remove('added__cart--active');
 }
 
@@ -72,7 +87,6 @@ function addItem(e) {
     const wholesale = $(wrap).find('[data-wholesale]');
     const qty = $(wrap).find('.product__qtyNum').val();
 
-    // console.log($(listing).attr('data-listing'));
     // Create obj with data atr info
     const product = {
         img: $(img).attr('src'),
@@ -84,7 +98,6 @@ function addItem(e) {
         qty: qty
     };
 
-
     // HTML template created in My Cart
     const mycart__list = document.querySelector('.mycart__list');
 
@@ -92,7 +105,7 @@ function addItem(e) {
             <div class="mycart__item">
                 <div class="mycart__close"><i class="fas fa-times"></i></div>
                 <div class="product__brand">${product.brand} <span class="product__subbrand">${product.subbrand}</span></div>
-                <div class="product__wholesale">${product.wholesale}</div>
+                <div class="product__wholesale">${formatter.format(product.wholesale * product.qty)}</div>
                 <div class="product__qty">
                 <input type="number" class="product__qtyNum" value="${product.qty}" min="0">
                 </div>
@@ -101,26 +114,65 @@ function addItem(e) {
 
     mycart__list.insertAdjacentHTML('afterbegin', html);
 
+    const addedCart__list = document.querySelector('.added__cart');
+
+    const hoverHtml = `
+            <div class="added__item">
+                <div class="product__img"><img src=${product.img} alt="sat"></div>
+                <div class="product__brand">${product.brand} <span class="product__subbrand">${product.subbrand}</span></div>
+                <div class="product__itemNo">${product.itemno}</div>
+            </div>
+    `;
+
+    addedCart__list.insertAdjacentHTML('afterbegin', hoverHtml);
+
     // Object/Product go in Array
     allProducts.push(product);
-    console.log(allProducts);
 
     // Change button
     const btn = document.querySelector('.addBtn');
     btn.classList.remove('addBtn');
     btn.className = 'addedBtn';
     btn.innerText = 'ADDED'
+
+    showTotal();
 }
 
 // Remove items from cart list 
 
 function removeItem(e) {
-    const itemList = this.children;
-    const item = Array.from(itemList);
+    const el = e.target;
+    const cartItem = el.parentElement.parentElement;
+    const cartList = el.parentElement.parentElement.parentElement;
 
-    if (e.target.classList.contains('mycart__close')) {
-        const list = e.target.parentElement.parentElement;
-        const selectedItem = e.target.parentElement;
-        list.removeChild(selectedItem);
+    if (el.parentElement.classList.contains('mycart__close')) {
+        cartList.removeChild(cartItem);
     }
+    allProducts.shift(el);  // TODO remove current element, not 1st or last
+    showTotal();
+}
+
+// Show total
+
+function showTotal() {
+    const total = [];
+
+    allProducts.forEach(product => {
+        const price = product.listing;
+        const qty = product.qty;
+        total.push(parseFloat(price*qty));
+    });
+
+    const totalMoney = total.reduce((total, price) => {
+        total += price;
+        return total;
+    }, 0);
+    
+    const finalMoney = formatter.format(totalMoney);
+
+    totalTop.textContent = finalMoney;
+    totalBottom.textContent = finalMoney;
+    totalItem.textContent = total.length;
+
+    return total;
 }
